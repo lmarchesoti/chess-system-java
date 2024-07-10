b307a06d95f796d34339c1e3c4579d42aed55e45
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2024-07-10 15:44:31 -0300

    Add BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,10 @@ public class Board {
     private final Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
+
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -20,15 +24,49 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
+        validatePositionExists(row, column);
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        validatePositionExists(position);
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        validatePositionAvailable(position);
+
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    private void validatePositionExists(int row, int column) {
+        if (!positionExists(row, column)) {
+            throw new BoardException("Position does not exist");
+        }
+    }
+
+    private void validatePositionExists(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position does not exist");
+        }
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        return piece(position) != null;
+    }
+
+    private void validatePositionAvailable(Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece in position " + position);
+        }
     }
 }
